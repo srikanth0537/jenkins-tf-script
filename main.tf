3157295091814faa3437749b32835b7ba511e2ab
srikanth0537 <107805310+srikanth0537@users.noreply.github.com>
2023-05-15 19:27:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,10 +6,9 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  count = 2
 
   tags = {
-    Name = format("VM_%d", count.index)
+    Name = "sample-web-server"
   }
 
 }
